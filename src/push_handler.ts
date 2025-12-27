d8be631238341eca96c34fe6eb5015d947e623b4
kenshoen <240387523+kenshoen@users.noreply.github.com>
2025-12-28 06:10:08 +0900

    feat: spawn scripts instead of awaiting

--- src/push_handler.ts
+++ src/push_handler.ts
@@ -1,6 +1,6 @@
 import { exists } from "node:fs/promises"
 import type { EmitterWebhookEvent, Webhooks } from "@octokit/webhooks"
-import { $ } from "bun"
+import { spawn } from "bun"
 
 /**
  * Handles webhook requests from GitHub
@@ -17,34 +17,34 @@ export class PushHandler {
     /**
      * Handles push events from GitHub. Runs update script for the specified ref if it's configured.
      */
-    private async pullRepo(event: EmitterWebhookEvent<"push">): Promise<void> {
+    private pullRepo(event: EmitterWebhookEvent<"push">): void {
         const { ref, before, after } = event.payload
 
         const path = this.paths.get(ref.replace(/^refs\/heads\//, ""))
         if (!path) {
-            console.info(`Not configured for ${ref}; Ignoring`)
+            console.info(`Not configured for ${ref} Ignoring`)
             return
         }
 
         console.info(`Running update script for ${ref}`)
-        await this.runUpdate(path, ref, before, after)
+        this.runUpdate(path, ref, before, after)
     }
 
     /**
      * Runs update script for the specified path
      */
-    private async runUpdate(
+    private runUpdate(
         script: string,
         ref: string,
         before: string,
         after: string,
-    ): Promise<void> {
+    ): void {
         if (!exists(script)) {
             throw Error(`Script ${script} does not exist`)
         }
         console.info({
-            running: `/bin/bash -- ${script} ${ref} ${before} ${after}`,
+            spawn: `/bin/bash -- ${script} ${ref} ${before} ${after}`,
         })
-        await $`/bin/bash -- ${script} ${ref} ${before} ${after}`
+        spawn([`/bin/bash`, "--", script, ref, before, after]).unref()
     }
 }
